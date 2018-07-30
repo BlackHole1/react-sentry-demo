bd1ace60a9f791186c86e53271d0d9ed4006e69d
BlackHole1 <158blackhole@gmail.com>
2018-07-30 14:06:20 +0800

    Add code annotation

--- src/SentryBoundary.js
+++ src/SentryBoundary.js
@@ -9,12 +9,14 @@ export default class SentryBoundary extends Component {
 
   componentDidCatch(error, errorInfo) {
     this.setState({ error });
+    // 发送错误信息
     Raven.captureException(error, { extra: errorInfo });
   }
 
-render() {
+  render() {
     if (this.state.error) {
-      console.log('React Error')
+      // 此处可以写成组件，当组件崩溃后，可以替换崩溃的组件
+      console.log("React Error");
     }
     return this.props.children;
   }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import Raven from "raven-js";
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import SentryBoundary from './SentryBoundary';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import SentryBoundary from "./SentryBoundary";
 
-Raven.config('https://bea834ff9ce34d329762dca3117ce34d@sentry.io/1248812', {
+Raven.config("https://bea834ff9ce34d329762dca3117ce34d@sentry.io/1248812", {
   environment: process.env.NODE_ENV,
+  // 此处可以放一些具有标志的信息，用于后期再平台在查找某一处指定问题
+  // 比如当前版本号
   tags: {
-    version: '1.0.0'
+    version: "1.0.0"
   },
   shouldSendCallback: function(data) {
-    return (data.environment === "development");
+    // 如果当前环境是development，则发送错误报错。如果不是则不发送
+    return data.environment === "development";
   }
 }).install();
 
-ReactDOM.render((
+ReactDOM.render(
   <div>
     <SentryBoundary>
       <App />
     </SentryBoundary>
-  </div>
-), document.getElementById('root'));
+  </div>,
+  document.getElementById("root")
+);
 registerServiceWorker();
