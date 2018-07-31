027cd0f52353323f60867e36eb7a1ee172b8feb1
BlackHole1 <158blackhole@gmail.com>
2018-07-31 20:12:17 +0800

    Update README.md and Optimization code

--- scripts/transform-release.js
+++ scripts/transform-release.js
@@ -1,21 +1,9 @@
-const { writeFile, writeFileSync, readFileSync } = require("fs");
+const { writeFile } = require("fs");
 const { release } = require("../package.json");
 
-// 从package获取release信息，并写入到release.json文件
-writeFile("./release.json", `{"release": "${release}"}`, err => {
+// 从package获取release信息，并写入到src目录下的release.json文件
+writeFile("./src/release.json", `{"release": "${release}"}`, err => {
   if (err) {
-    throw Error(`无法把release写入到release.json文件里，错误输出: ${err}`);
+    throw Error(`无法把release写入到src/release.json文件里，错误输出: ${err}`);
   }
-
-  // 复制release.json文件到src目录下，用于应用获取
-  copyFile("./release.json", "./src/release.json");
 });
-
-/**
- * 复制文件到指定目录
- * @param {*} src 源文件
- * @param {*} dist 将要复制的地方
- */
-function copyFile(src, dist) {
-  writeFileSync(dist, readFileSync(src));
-}
